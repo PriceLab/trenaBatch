dc15e86ba1cfdeaa2facf1f02b9c74f30cc8e068
paul-shannon <paul.thurmond.shannon@gmail.com>
2019-02-08 09:24:29 -0800

    uses new tbl.geneInfo from ensembl gtf

--- AD/footprints/config.R
+++ AD/footprints/config.R
@@ -18,8 +18,9 @@ mtx <- getExpressionMatrix(trenaProject, matrix.name)
 
 stopifnot(grepl("ENSG", rownames(mtx)[1]))
 
-print(load(system.file(package="TrenaProject", "extdata", "geneInfoTable.RData")))
-stopifnot(exists("tbl.geneInfo"))
+# print(load(system.file(package="TrenaProject", "extdata", "geneInfoTable.RData")))
+# stopifnot(exists("tbl.geneInfo"))
+print(load(system.file(package="TrenaProject", "extdata", "geneInfoTable_hg38.RData")))
 
 failures <- which(nchar(tbl.geneInfo$hgnc_symbol) == 0)
 length(failures)
@@ -27,15 +28,16 @@ length(failures)
 dim(mtx)
 dim(tbl.geneInfo)
 all(rownames(mtx) %in% tbl.geneInfo$ensg)
-no.transcript.ensgs <- setdiff(rownames(mtx), unique(tbl.geneInfo$ensg))  # [1] 61
+no.transcript.ensgs <- setdiff(rownames(mtx), unique(tbl.geneInfo$ensg))
+length(no.transcript.ensgs) # 61
 
-print(load(system.file(package="TrenaProject", "extdata", "epigenome", "geneHancer.v4.7.allGenes.RData"))) # 61
+print(load(system.file(package="TrenaProject", "extdata", "genomeAnnotation", "geneHancer.v4.7.allGenes.RData"))) # 61
 dim(tbl.enhancers)
 geneSymbols.with.enhancers <- intersect(tbl.geneInfo$geneSymbol, unique(tbl.enhancers$geneSymbol))
 printf("geneSymbols.with.enhancers: %d", length(geneSymbols.with.enhancers)) # 15294
 
 ensg.with.enhancers <- subset(tbl.geneInfo, geneSymbol %in% geneSymbols.with.enhancers)$ensg
-length(unique(ensg.with.enhancers))  # 15295, no dups
+length(unique(ensg.with.enhancers))  # 33818, no dups
 ensg.without.enhancers <- setdiff(rownames(mtx), ensg.with.enhancers)
 length(ensg.without.enhancers)       # 1708
 
@@ -44,6 +46,6 @@ length(setdiff(rownames(mtx), ensg.with.enhancers))   # 1708/17003 = 15295 mtx e
 goi.test <- c(ensg.with.enhancers[1], ensg.without.enhancers[1])
 goi <- rownames(mtx)
 #goi <- c(head(rownames(mtx), n=39), "fubar")
-#goi <- "ENSG00000227232"
+goi <- "ENSG00000002587"
 printf("established %d goi", length(goi))
 configurationFileRead <- TRUE
