8329f9143952a11af50b6427b1489e5951445607
paul-shannon <paul.thurmond.shannon@gmail.com>
2019-02-13 16:28:35 -0800

    reports measured/described numbers by tissue

--- AD/footprints/GTEx_parsing.R
+++ AD/footprints/GTEx_parsing.R
@@ -3,14 +3,15 @@
 # load the matrix
 mtx <- readRDS("GeneReads.rds")
 
-print(load("matrix.colnames.RData"))
-length(matrix.colnames)
-head(matrix.colnames)
+# print(load("matrix.colnames.RData"))
+# length(matrix.colnames)
+# head(matrix.colnames)
 tbl.md <- read.table("GTEx_v7_Annotations_SampleAttributesDS.txt", sep = '\t', header=TRUE,
                      quote="", as.is=TRUE, row.names=1)
 
 dim(tbl.md) # 15598 63
 
+matrix.colnames <- colnames(mtx)
 length(intersect(matrix.colnames, rownames(tbl.md)))
 
 new.colnames <- gsub(".", "-", matrix.colnames, fixed=TRUE)
@@ -28,12 +29,25 @@ tail(tbl.tissues)
 
 # get the Whole_blood samples (total of 2412)
 blood.sampleIDs <- rownames(subset(tbl.md, SMTSD=="Whole_Blood"))
-intersect(blood.sampleIDs, new.colnames)
-length(blood.sampleIDs) # 2412 
+length(blood.sampleIDs)
+length(intersect(blood.sampleIDs, colnames(mtx)))
+
+length(blood.sampleIDs) # 2412
 
 blood.intersect <- which(blood.sampleIDs %in% colnames(mtx))
 length(blood.intersect) # 407
 
+described.count <- 0
+measured.count <- 0
 
+for(tissueName in tbl.tissues$tissue){
+   samples.described <- rownames(subset(tbl.md, SMTSD==tissueName))
+   samples.measured <- length(intersect(samples.described, colnames(mtx)))
+   described.count <- described.count + length(samples.described)
+   measured.count <- measured.count + samples.measured
+   printf("tissueName: %s: %d/%d", tissueName, samples.measured, length(samples.described))
+   }
+
+printf("described: %d   measured:  %d", described.count, measured.count)
 # extract from the big all-tissue matrix
 #mtx.blood <- mtx[, blood.sampleIDs]
